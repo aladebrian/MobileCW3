65c423f215e437cb1584e7bc510806cf1d7be669
Brian <aladebrian@gmail.com>
2025-02-24 13:08:56 -0500

    buggy layout finished

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const TaskListScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TaskListScreen> createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  List<String> tasks = [];
+  TextEditingController txt = TextEditingController();
+  void addTask() {
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(controller: txt),
+              ElevatedButton(onPressed: addTask, child: Text("Add Task"))
+
+            ],
+          ),
+        ]
+      )
+    );
+  }
+}
